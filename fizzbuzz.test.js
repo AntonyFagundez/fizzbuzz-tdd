ed107a2936bbfb3191c83cae1c3b6493c3e38917
Antony Fagundez <jhonfagundez@gmail.com>
2021-03-28 18:51:06 -0300

    corrected test descriptions

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -3,7 +3,7 @@ const { FIZZ, BUZZ, FIZZBUZZ } = require('./constants');
 
 
 describe('fizzbuzz', () => {
-    test('test', () => {
+    test('dummy test', () => {
         expect(true).toBe(true);
     });
 
@@ -25,37 +25,37 @@ describe('fizzbuzz', () => {
         }).toThrowError("The argument must be a number");
     });
 
-    test('should print 0 if they recive 0',()=>{
+    test('should return 0 if they recive 0',()=>{
         const expected = 0;
         const result = fizzbuzz(0);
         expect(result).toBe(expected);
     })
 
-    test('should print if they recive 1', () => {
+    test('should return 1 if they recive 1', () => {
         const expected = 1;
         const result = fizzbuzz(1);
         expect(result).toBe(expected);
     });
 
-    test(`should print '${FIZZ}' if they receive 3`, () => {
+    test(`should return '${FIZZ}' if they receive 3`, () => {
         const expected = FIZZ;
         const result = fizzbuzz(3);
         expect(result).toBe(expected);
     });
 
-    test(`should print '${FIZZ}' if they receive 3`, () => {
+    test(`should return '${FIZZ}' if they receive a multiple of 3`, () => {
         const expected = FIZZ;
         const result = fizzbuzz(6);
         expect(result).toBe(expected);
     });
 
-    test(`should print '${BUZZ}' if they receive 5`, () => {
+    test(`should return '${BUZZ}' if they receive a multiple of 5`, () => {
         const expected = BUZZ;
         const result = fizzbuzz(5);
         expect(result).toBe(expected);
     });
 
-    test(`should print '${FIZZBUZZ}' if they receive a multiple of 3 and 5`, () => {
+    test(`should return '${FIZZBUZZ}' if they receive a multiple of 3 and 5`, () => {
         const expected = FIZZBUZZ;
         const result = fizzbuzz(15);
         expect(result).toBe(expected);
